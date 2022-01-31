b4e59def899a57bef88377b4a4a3360d9078545a
zeektenka <zeektenka@gmail.com>
2022-01-31 10:16:12 +0200

    1 file modified

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const HighScore = document.getElementById('high_score');
-const Score = document.getElementById('score');
-const HighScore2 = document.getElementById('high_score2');
-const Score2 = document.getElementById('score2');
+let HighScore = document.getElementById('high_score');
+let Score = document.getElementById('score');
+let HighScore2 = document.getElementById('high_score2');
+let Score2 = document.getElementById('score2');
 const startScreen = document.getElementById('startScreen');
 const StartBtn = document.getElementById('start_btn');
 const GameOver = document.getElementById('game_over');
@@ -15,6 +15,12 @@ StartBtn.addEventListener('click', function () {
 
 PlayAgainBtn.addEventListener('click', function () {
   GameOver.style.display = 'none';
+  score_count = 0;
+  Score.innerText = 0;
+  high_score =
+    localStorage.getItem('HighScore') &&
+    JSON.parse(localStorage.getItem('HighScore'));
+  HighScore.innerText = high_score;
   Game();
 });
 
@@ -100,19 +106,19 @@ function Game() {
   ///////////////
 
   let bulletsArray = [];
-  let bullet = new Bullets(25, 0, 0);
+  //let bullet = new Bullets(25, 0, 0);
 
   let enemyArr = [];
   let debris = [];
 
   function SpawnEnemies() {}
   for (let i = 0; i < 10; i++) {
-    const radius = (Math.random() + 3) * 10;
-    const color = `rgb(${Math.random() * 255},${Math.random() * 255},${
+    let radius = (Math.random() + 3) * 10;
+    const color = `rgb(${(Math.random() + 5) * 255},${Math.random() * 255},${
       Math.random() * 255
     })`;
     let xMax = canvas.width + 800;
-    let xMin = -800;
+    let xMin = -900;
     let yMax = canvas.height + 800;
     let yMin = -800;
 
@@ -145,7 +151,7 @@ function Game() {
     let vx = newX / distance;
     let vy = newY / distance;
 
-    bulletsArray.push(new Bullets(5, vx, vy));
+    bulletsArray.push(new Bullets(6, vx, vy));
   }
 
   document.addEventListener('click', (e) => {
@@ -154,7 +160,7 @@ function Game() {
   let animationId;
   function animate() {
     animationId = requestAnimationFrame(animate); // Create an animation loop
-    c.fillStyle = ' rgba(0,0,0,0.5)';
+    c.fillStyle = ' rgba(0,0,0,0.3)';
     c.fillRect(0, 0, canvas.width, canvas.height); // Erase whole canvas
 
     drawPlayer();
@@ -186,6 +192,7 @@ function Game() {
       const dist = Math.hypot(enemy.x - x, enemy.y - y);
       if (dist - enemy.radius - Playerradius < 1) {
         Score2.innerText = score_count;
+        HighScore2 = high_score;
         cancelAnimationFrame(animationId);
         GameOver.style.display = 'grid';
       }
@@ -196,10 +203,14 @@ function Game() {
         if (dist - enemy.radius < 1) {
           score_count += 5;
           Score.innerText = score_count;
+          if (score_count > high_score) {
+            high_score = score_count;
+            localStorage.setItem('HighScore', JSON.stringify(high_score));
+          }
           for (let i = 0; i < 2; i++) {
-            const radius = (Math.random() + 3) * 10;
+            let radius = (Math.random() + 3) * 10;
             const color = `rgb(${Math.random() * 255},${Math.random() * 255},${
-              Math.random() * 255
+              (Math.random()+5) * 255
             })`;
             let xMax = canvas.width + 800;
             let xMin = -800;
@@ -221,25 +232,22 @@ function Game() {
 
             enemyArr.push(new Objects(x, y, radius, color, vx, vy));
           }
-          if (score_count > high_score) {
-            high_score = score_count;
-            localStorage.setItem('HighScore', JSON.stringify(high_score));
-          }
-          for (let i = 0; i < 8; i++) {
+
+          for (let i = 0; i < 200; i++) {
             debris.push(
               new Objects(
                 enemy.x,
                 enemy.y,
                 2,
                 enemy.color,
-                (Math.random() - 0.5) * 60,
-                (Math.random() - 0.5) * 60
+                (Math.random() - 0.5) * 100,
+                (Math.random() - 0.5) * 100
               )
             );
           }
           enemy.radius = enemy.radius - 10;
           setTimeout(() => {
-            enemy.radius < 25 && enemyArr.splice(EnemyIndex, 1);
+            enemy.radius < 20 && enemyArr.splice(EnemyIndex, 1);
             bulletsArray.splice(bulletIndex, 1);
           }, 0);
         }
